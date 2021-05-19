7f8bc837b7500dcfc04a7bb5f983397aa425b6a6
lilian Robert <mslillianito@gmail.com>
2021-05-19 00:00:10 -0700

    more functionality

--- books_authors/urls.py
+++ books_authors/urls.py
@@ -2,4 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('add_book', views.add_book),
+    path('view_book', views.view_book),
+    path('add_author', views.add_author),
+    path('add_book_author', views.add_book_author),
+    path('view_author', views.view_author),
+    path('add_book', views.add_book),
+    path('add_author_book', views.add_author_book),
 ]
--- books_authors/views.py
+++ books_authors/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 from .models import Book, Author
+
 # Create your views here.
 def index(request):
-    return HttpResponse("we are here")
+    return render(request, "index.html", {"books": Book.objects.all()})
+def add_book(request):
+    Book.objects.create(title= request.POST['title'], desc= request.POST['desc'])
+    return redirect("/")
+# def view_book(request):
+#     book = Book.objects.get(id= book_id)
+#     context = {
+#         "book": book,
+#         "authors": Author.objects.exclude(books__id= book_id)
+#     }
+#     return render(request,'book.html', context)
