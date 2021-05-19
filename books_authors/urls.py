ae61c4fa6b13567faa21fb7d8f38c358ac43c687
lilian Robert <mslillianito@gmail.com>
2021-05-19 01:47:58 -0700

    added book detail view rendering

--- books_authors/urls.py
+++ books_authors/urls.py
@@ -3,10 +3,10 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('add_book', views.add_book),
-    path('view_book', views.view_book),
-    path('add_author', views.add_author),
-    path('add_book_author', views.add_book_author),
-    path('view_author', views.view_author),
-    path('add_book', views.add_book),
-    path('add_author_book', views.add_author_book),
+    path('view_book/<int:book_id>', views.view_book),
+    # path('add_author', views.add_author),
+    path('add_book_author/<int:book_id>', views.add_book_author),
+    # path('view_author', views.view_author),
+    # path('add_book', views.add_book),
+    # path('add_author_book', views.add_author_book),
 ]
--- books_authors/views.py
+++ books_authors/views.py
@@ -7,10 +7,15 @@ def index(request):
 def add_book(request):
     Book.objects.create(title= request.POST['title'], desc= request.POST['desc'])
     return redirect("/")
-# def view_book(request):
-#     book = Book.objects.get(id= book_id)
-#     context = {
-#         "book": book,
-#         "authors": Author.objects.exclude(books__id= book_id)
-#     }
-#     return render(request,'book.html', context)
+def view_book(request, book_id):
+    book = Book.objects.get(id= book_id)
+    context = {
+        "book": book,
+        "authors": Author.objects.exclude(books__id= book_id)
+    }
+    return render(request, "book.html", context)
+def add_book_author(request, book_id):
+    book = Book.objects.get(id= book_id)
+    author = Author.objects.get(id=request.POST['author_id'])
+    book.authors.add(author)
+    return redirect(f'add_book/{book_id}')
